686d4fbe41737c3076d7a8c78347cec18a7b96d1
tanhung2312 <ngotanhung917@gmail.com>
2026-03-08 17:35:07 +0700

    Update cart.js
    feat: thêm, xóa và cập nhật số lượng trong giỏ hàng

--- be/cart.js
+++ be/cart.js
@@ -4,3 +4,12 @@ const addToCart = (product) => {
     cart.push(product);
     console.log("Đã thêm:", product);
 };
+
+const removeItem = (id) => {
+    cart = cart.filter(item => item.id !== id);
+};
+
+const updateQty = (id, qty) => {
+    let item = cart.find(i => i.id === id);
+    if (item) item.quantity = qty;
+};
